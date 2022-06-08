179ba704e4fdec6af67ea6b405e3bde49ed70c2c
guanCompany <934750771@qq.com>
2022-06-08 17:17:55 +0800

    引入 tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <h2 className="text-red-800 bg-gray-200 w-[100px]">6666</h2>
     </div>
   );
 }
