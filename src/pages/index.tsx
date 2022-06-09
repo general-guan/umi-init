571068a53d81513db963e04e51c335924f9f980d
guanCompany <934750771@qq.com>
2022-06-09 22:01:01 +0800

    tailwindcss 环境更改

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
-      <h2 className="text-red-300 bg-gray-500 w-[300px] text-right">6666</h2>
+      <h2 className="text-red-500 bg-gray-400 w-[300px] text-right">6666</h2>
     </div>
   );
 }
